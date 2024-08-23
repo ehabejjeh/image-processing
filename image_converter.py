a90458876a50f9c6fd6a96d81bc1ee638dd8de8a
ehabejjeh <ehabejjeh@gmail.com>
2024-08-23 23:07:54 +0300

    writing the converter function first version

--- image_converter.py
+++ image_converter.py
@@ -1,2 +1,9 @@
 from PIL import Image
 from pathlib import Path
+
+
+def png_to_jpg():
+    for filename in Path("jpgImages").iterdir():
+        if filename.suffix == ".jpg":
+            new_filename = filename.with_suffix(".png")
+            print(new_filename)
